02b896049e84849a9edf8572df312c4a2d694b47
Mymy0202 <162684004+Mymy0202@users.noreply.github.com>
2024-10-16 14:58:00 +0800

    Add files via upload

---
+++ mymy2/src/main/java/com/mycompany/mymy2/Mymy2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mymy2;
+
+/**
+ *
+ * @author CL3-PC09
+ */
+public class Mymy2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
